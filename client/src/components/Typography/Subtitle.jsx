24b62c9c849bc3a1082e32377c7a09f9adf6d669
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-24 02:35:08 +0200

    feat: add subtitle component

---
+++ client/src/components/Typography/Subtitle.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Subtitle({ children }) {
+  return <h2 className='font-poppins mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300'>{children}</h2>;
+}
