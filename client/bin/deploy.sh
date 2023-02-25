4c49f859c859fcb4423054422ee17cc30ebe491d
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-25 18:25:16 +0200

    chore: fix add env vars commands

--- client/bin/deploy.sh
+++ client/bin/deploy.sh
@@ -1,15 +1,15 @@
-echo "Deploying client app"
-
 # link project to vercel 
 echo "Link app to vercel project"
 vercel link --token=$VERCEL_TOKEN --project=$VERCEL_CLIENT_PROJECT --scope=$VERCEL_SCOPE --yes
 
 # set application env variables
-vercel env add VITE_BASE_URL=$VITE_BASE_URL --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
-vercel env add VITE_AUTH0_DOMAIN=$VITE_AUTH0_DOMAIN --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
-vercel env add VITE_AUTH0_CLIENT_ID=$VITE_AUTH0_CLIENT_ID --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
-vercel env add VITE_AUTH0_REDIRECT_URI=$VITE_AUTH0_REDIRECT_URI --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
-vercel env add VITE_AUTH0_API_AUDIENCE=$VITE_AUTH0_API_AUDIENCE --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
+echo "Add app env variables"
+echo $VITE_BASE_URL | vercel env add VITE_BASE_URL production --token=$VERCEL_TOKEN 
+echo $VITE_AUTH0_DOMAIN | vercel env add VITE_AUTH0_DOMAIN production --token=$VERCEL_TOKEN
+echo $VITE_AUTH0_CLIENT_ID | vercel env add VITE_AUTH0_CLIENT_ID production --token=$VERCEL_TOKEN
+echo $VITE_AUTH0_REDIRECT_URI | vercel env add VITE_AUTH0_REDIRECT_URI production --token=$VERCEL_TOKEN
+echo $VITE_AUTH0_API_AUDIENCE | vercel env add VITE_AUTH0_API_AUDIENCE production --token=$VERCEL_TOKEN
 
-# deploy tp project
+# deploy to project
+echo "Deploying client app"
 vercel deploy dist/ --token=$VERCEL_TOKEN
--- server/bin/deploy.sh
+++ server/bin/deploy.sh
@@ -1,18 +1,17 @@
-echo "Deploying server app"
-
 # link app to vercel project
 echo "Link app to vercel project"
 vercel link --token=$VERCEL_TOKEN --project=$VERCEL_SERVER_PROJECT --scope=$VERCEL_SCOPE --yes
 
 # set application env variables
 echo "Add app env variables"
-vercel env add PORT=$PORT --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
-vercel env add NODE_ENV=$NODE_ENV --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
-vercel env add MONGO_USER=$MONGO_USER --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
-vercel env add MONGO_CLUSTER=$MONGO_CLUSTER --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
-vercel env add MONGO_DATABASE=$MONGO_DATABASE --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
-vercel env add MONGO_USER_PASSWORD=$MONGO_USER_PASSWORD --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes
+echo $PORT | vercel env add PORT production --token=$VERCEL_TOKEN
+echo $NODE_ENV | vercel env add NODE_ENV production --token=$VERCEL_TOKEN
+echo $MONGO_USER | vercel env add MONGO_USER production --token=$VERCEL_TOKEN
+echo $MONGO_CLUSTER | vercel env add MONGO_CLUSTER production --token=$VERCEL_TOKEN
+echo $MONGO_DATABASE | vercel env add MONGO_DATABASE production --token=$VERCEL_TOKEN
+echo $MONGO_USER_PASSWORD | vercel env add MONGO_USER_PASSWORD production --token=$VERCEL_TOKEN
 
 # deploy tp project
+echo "Deploying server app"
 vercel deploy dist/ --token=$VERCEL_TOKEN
 
