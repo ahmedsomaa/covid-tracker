035b4615badfc5f15a1c3f7df856f77647dcbf4c
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-25 15:34:40 +0200

    feat: create custom http request client

---
+++ client/src/utils/http.js
@@ -0,0 +1,25 @@
+export default function httpClient(enpoint, { method, requestHeaders, body } = {}) {
+  const defaultHeaders = { 'Content-Type': 'application/json' };
+  const config = {
+    method,
+    headers: {
+      ...defaultHeaders,
+      ...requestHeaders
+    }
+  };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  return fetch(enpoint, config)
+    .then(async (response) => {
+      if (response.ok) {
+        return await response.json();
+      } else {
+        const error = await response.json();
+        return Promise.reject(new Error(error.message));
+      }
+    })
+    .catch((err) => Promise.reject(new Error(err.message)));
+}
