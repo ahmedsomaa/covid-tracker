d36db9d7c72adf43dad22e48902d561a974518b0
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-25 15:35:44 +0200

    refactor: add backend server base url

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,5 +1,6 @@
 export default {
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  baseUrl: import.meta.env.VITE_BASE_URL,
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
   audience: import.meta.env.VITE_AUTH0_API_AUDIENCE,
   redirectUri: import.meta.env.VITE_AUTH0_REDIRECT_URI,
