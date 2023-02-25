cc9d6993d267876566d5f5a94ca2aec712cb53f4
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-25 15:37:14 +0200

    refactor: integrate create record api

--- client/src/pages/CovidCheck.jsx
+++ client/src/pages/CovidCheck.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import Check from '../icons/Check';
+import XMark from '../icons/XMark';
+import { createRecord } from '../api';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 import Title from '../components/Typography/Title';
 import Subtitle from '../components/Typography/Subtitle';
-import { Label, TextInput, Card, ToggleSwitch, Button } from 'flowbite-react';
+import { Label, TextInput, Card, ToggleSwitch, Button, Toast } from 'flowbite-react';
 
 export default function CovidCheckPage() {
   const { user } = useAuth0();
   const navigate = useNavigate();
   const latitudeRef = React.useRef();
   const longtiudeRef = React.useRef();
+  const [req, setReq] = React.useState({ toast: '', load: false });
   const [form, setForm] = React.useState({
     temperature: 28,
     fatigue: false,
@@ -47,7 +51,7 @@ export default function CovidCheckPage() {
     const { temperature, ...symptomps } = form;
 
     // user, temp, location, sysmptops
-    const request_body = {
+    const record = {
       patientId: user.sub,
       location,
       temperature: parseInt(temperature),
@@ -55,7 +59,12 @@ export default function CovidCheckPage() {
     };
 
     // call api service here
-    console.log(request_body);
+    setReq({ ...req, load: true });
+    createRecord(record).then(
+      (_data) => setReq({ ...req, toast: 'INF,Successfully stored user record' }),
+      (_err) => setReq({ ...req, toast: 'ERR,Failed to store user record' })
+    );
+    setReq({ ...req, load: false });
   };
 
   const onCancel = () => navigate('/app/dashboard');
@@ -65,6 +74,22 @@ export default function CovidCheckPage() {
   return (
     <>
       <Title>Covid Check</Title>
+
+      {req.toast && (
+        <Toast className='my-3'>
+          {req.toast.split(',')[0] === 'INF' ? (
+            <div className='inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-blue-100 text-blue-500 dark:bg-blue-800 dark:text-blue-200'>
+              <Check className='h-5 w-5' />
+            </div>
+          ) : (
+            <div className='inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500 dark:bg-red-800 dark:text-red-200'>
+              <XMark className='h-5 w-5' />
+            </div>
+          )}
+          <div className='ml-3 text-sm font-normal'>{req.toast.split(',')[1]}</div>
+          <Toast.Toggle />
+        </Toast>
+      )}
 
       <Card className='font-serif flex flex-col w-full py-2 px-4 shadow-none'>
         <form onSubmit={onSubmit} className='space-y-4 md:space-y-6'>
@@ -86,6 +111,7 @@ export default function CovidCheckPage() {
               min={9}
               max={42}
               required
+              step='any'
               type='number'
               id='temperature'
               value={form.temperature}
@@ -177,10 +203,10 @@ export default function CovidCheckPage() {
           </div>
           <hr />
           <div className='flex flex-row-reverse gap-2'>
-            <Button type='submit' color='success'>
-              Check
+            <Button type='submit' color='success' disabled={req.load}>
+              {!req.load ? 'Check' : 'Checking...'}
             </Button>
-            <Button type='button' color='gray' onClick={onCancel}>
+            <Button type='button' color='gray' onClick={onCancel} disabled={req.load}>
               Cancel
             </Button>
           </div>
