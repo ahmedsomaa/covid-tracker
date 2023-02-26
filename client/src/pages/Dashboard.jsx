9bdf99b85167c965e3694f0c83c44d3e95c7d7b0
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-27 00:28:50 +0200

    fix: add tooltip

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,8 +5,10 @@ import { getAllRecords } from '../api';
 import CovidMap from '../components/CovidMap';
 import Redirect from '../components/Redirect';
 import Title from '../components/Typography/Title';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
 
 export default function DashboardPage() {
+  const [tooltipContent, setTooltipContent] = React.useState(null);
   const [request, setRequest] = React.useState({
     data: [],
     error: '',
@@ -30,7 +32,12 @@ export default function DashboardPage() {
         <Redirect message='Please wait while we fetch the records' />
       ) : (
         <Card className='font-serif flex flex-col w-full py-2 px-4 shadow-none'>
-          <CovidMap geoUrl={config.mapGeoUrl} data={request.data} />
+          <CovidMap geoUrl={config.mapGeoUrl} data={request.data} setTooltipContent={setTooltipContent} />
+          {tooltipContent && (
+            <ReactTooltip anchorSelect='.map-tooltip' place='top'>
+              {tooltipContent}
+            </ReactTooltip>
+          )}
         </Card>
       )}
     </>
