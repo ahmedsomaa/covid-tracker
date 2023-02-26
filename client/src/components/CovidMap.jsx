192599af69e765b850e3db51d9e4df286a4cda24
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-27 00:28:38 +0200

    fix: color regions by cases and integrate tooltip

--- client/src/components/CovidMap.jsx
+++ client/src/components/CovidMap.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
 import config from '../config';
 import PropTypes from 'prop-types';
-import { ComposableMap, Geographies, Geography, Marker } from 'react-simple-maps';
+import { scaleLinear } from 'd3-scale';
+import { ComposableMap, Geographies, Geography, Sphere, Graticule } from 'react-simple-maps';
 
-export default function CovidMap({ data, geoUrl }) {
+export default function CovidMap({ data, geoUrl, setTooltipContent }) {
+  const colorScale = scaleLinear()
+    .domain([1, Math.max(...data.map((d) => d.cases))])
+    .range(['#fef2f2', '#7f1d1d']);
+
   return (
     <>
-      <ComposableMap>
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => <Geography fill='#f8fafc' stroke='#475569' key={geo.rsmKey} geography={geo} />)
+      <ComposableMap
+        projectionConfig={{ scale: 148 }}
+        data-tip=''
+        onClick={(e) => {
+          // The map container has a width and the SVGs for countries do not.
+          // E.g., checks if the user is clicking on the map itself.
+          if (e.target.width) {
+            setTooltipContent(null);
           }
-        </Geographies>
-        {data.map(({ id, coords, infected }) => (
-          <Marker key={id} coordinates={coords}>
-            <circle r={3} fill={infected ? '#dc2626' : '#16a34a'} />
-          </Marker>
-        ))}
+        }}
+      >
+        <Sphere stroke='#475569' strokeWidth={0.5} />
+        <Graticule stroke='#475569' strokeWidth={0.5} />
+        {data.length > 0 && (
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const record = data.find((s) => s.iso3 === geo.id);
+                return (
+                  <Geography
+                    geography={geo}
+                    key={geo.rsmKey}
+                    stroke='#475569'
+                    className='map-tooltip'
+                    onMouseEnter={() => {
+                      const message = record
+                        ? `: ${record.cases} reported cases with avg. temp: ${record.temperature}°C `
+                        : '';
+                      setTooltipContent(`${geo.properties.name}${message}`);
+                    }}
+                    onMouseLeave={() => {
+                      setTooltipContent('');
+                    }}
+                    fill={record ? colorScale(record.cases) : '#f8fafc'}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        )}
       </ComposableMap>
     </>
   );
