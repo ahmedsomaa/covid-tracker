9537f3731a99833c15d6fca318ae181027dda30e
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-27 00:26:21 +0200

    fix: update findAll to return cases by country

--- server/src/controllers/records.js
+++ server/src/controllers/records.js
@@ -1,18 +1,33 @@
 import Record from '../models/record';
 import hasCovid from '../utils/hasCovid';
+import { iso1A3Code } from '@ideditor/country-coder';
 
 // ----------------------------------------- handlers
 const findAll = async (_request, response, next) => {
   try {
-    const records = await Record.find({}).select({ patientId: 1, location: 1, infected: 1 });
+    const records = await Record.aggregate([
+      {
+        $match: {
+          infected: {
+            $eq: true
+          }
+        }
+      },
+      {
+        $group: {
+          _id: '$countryCode',
+          cases: { $sum: 1 },
+          temperature: { $avg: '$temperature' }
+        }
+      }
+    ]);
     response
       .status(200)
       .json(
-        records.map(({ patientId, location, _id, infected }) => ({
-          id: _id,
-          patientId,
-          coords: [location.lng, location.lat],
-          infected
+        records.map(({ _id, temperature, ...rest }) => ({
+          iso3: _id,
+          temperature: parseFloat(temperature.toFixed(2)),
+          ...rest
         }))
       );
   } catch (error) {
@@ -23,11 +38,13 @@ const findAll = async (_request, response, next) => {
 const create = async (request, response, next) => {
   try {
     const { patientId, location, temperature, symptomps } = request.body;
+    const { lng, lat } = location;
     const newRecord = new Record({
       patientId,
       location,
       temperature,
       symptomps,
+      countryCode: iso1A3Code([lng, lat]) || 'NA',
       infected: hasCovid(temperature, symptomps)
     });
     const created = await newRecord.save();
