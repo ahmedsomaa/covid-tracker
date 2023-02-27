862c4d9360af08104abc41f9d6c271dabaabe0cf
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-27 15:18:55 +0200

    fix: update key names typo

--- client/src/icons/Check.jsx
+++ client/src/icons/Check.jsx
@@ -6,12 +6,12 @@ export default function Check({ ...props }) {
       {...props}
       fill='none'
       aria-hidden='true'
-      stroke-width='1.5'
+      strokeWidth='1.5'
       viewBox='0 0 24 24'
       stroke='currentColor'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <path d='M4.5 12.75l6 6 9-13.5' stroke-linecap='round' stroke-linejoin='round'></path>
+      <path d='M4.5 12.75l6 6 9-13.5' strokeLinecap='round' strokeLinejoin='round'></path>
     </svg>
   );
 }
--- client/src/icons/XMark.jsx
+++ client/src/icons/XMark.jsx
@@ -6,12 +6,12 @@ export default function XMark({ ...props }) {
       {...props}
       fill='none'
       aria-hidden='true'
-      stroke-width='1.5'
+      strokeWidth='1.5'
       viewBox='0 0 24 24'
       stroke='currentColor'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <path d='M6 18L18 6M6 6l12 12' stroke-linecap='round' stroke-linejoin='round'></path>
+      <path d='M6 18L18 6M6 6l12 12' strokeLinecap='round' strokeLinejoin='round'></path>
     </svg>
   );
 }
