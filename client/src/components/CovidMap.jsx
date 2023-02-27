e7e68ea756a1867237b76a40530a19a8bde6f9bc
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-27 15:19:58 +0200

    fix: update colorScale domain and plot using spred rate

--- client/src/components/CovidMap.jsx
+++ client/src/components/CovidMap.jsx
@@ -5,9 +5,7 @@ import { scaleLinear } from 'd3-scale';
 import { ComposableMap, Geographies, Geography, Sphere, Graticule } from 'react-simple-maps';
 
 export default function CovidMap({ data, geoUrl, setTooltipContent }) {
-  const colorScale = scaleLinear()
-    .domain([1, Math.max(...data.map((d) => d.cases))])
-    .range(['#fef2f2', '#7f1d1d']);
+  const colorScale = scaleLinear().domain([0, 1]).range(['#fca5a5', '#991b1b']);
 
   return (
     <>
@@ -15,8 +13,6 @@ export default function CovidMap({ data, geoUrl, setTooltipContent }) {
         projectionConfig={{ scale: 148 }}
         data-tip=''
         onClick={(e) => {
-          // The map container has a width and the SVGs for countries do not.
-          // E.g., checks if the user is clicking on the map itself.
           if (e.target.width) {
             setTooltipContent(null);
           }
@@ -36,15 +32,13 @@ export default function CovidMap({ data, geoUrl, setTooltipContent }) {
                     stroke='#475569'
                     className='map-tooltip'
                     onMouseEnter={() => {
-                      const message = record
-                        ? `: ${record.cases} reported cases with avg. temp: ${record.temperature}°C `
-                        : '';
+                      const message = record ? `: ${record.cases} infected cases out of ${record.total}` : '';
                       setTooltipContent(`${geo.properties.name}${message}`);
                     }}
                     onMouseLeave={() => {
                       setTooltipContent('');
                     }}
-                    fill={record ? colorScale(record.cases) : '#f8fafc'}
+                    fill={record ? colorScale(record.spreadRate) : '#f8fafc'}
                   />
                 );
               })
