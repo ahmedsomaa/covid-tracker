65974541f01edbe99148879e4ab355943e0a0d61
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-28 16:17:54 +0200

    refactor: check if error is invalid token or unauthorized error

--- server/src/middleware/error.js
+++ server/src/middleware/error.js
@@ -1,6 +1,17 @@
 import config from '../config';
+import { InvalidTokenError, UnauthorizedError } from 'express-oauth2-jwt-bearer';
 
 export default function errorHandler(error, _request, response, _next) {
+  if (error instanceof InvalidTokenError) {
+    const message = 'Bad Credentials';
+    return response.status(error.status).json({ message, success: false });
+  }
+
+  if (error instanceof UnauthorizedError) {
+    const message = 'Authentication Required';
+    return response.status(error.status).json({ message, success: false });
+  }
+
   const status = error.statusCode || 500;
   const stack = config.env === 'dev' ? error.stack : {};
   const message = error.message || 'Something went wrong';
