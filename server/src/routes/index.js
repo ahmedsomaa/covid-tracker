9b112b5342673b600ab9cc0482f32a2696617022
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-28 16:18:14 +0200

    feat: secure records route with auth0 middleware

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import recordsRouter from './records';
+import auth0Middleware from '../middleware/auth';
 
 const routes = Router();
 
-routes.use('/records', recordsRouter);
+routes.use(
+  '/records',
+  auth0Middleware.validateAuth0Token,
+  auth0Middleware.checkScopes,
+  recordsRouter
+);
 
 routes.get('/', (_req, res) => {
   res.status(200).send('<h1 style="text-align:center;">Covid Tracker API</h1>');
