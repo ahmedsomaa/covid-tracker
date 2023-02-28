8bf629ea5667e6b260961ab5f7dd4f585eec3b8f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-01 01:17:45 +0200

    feat: add constant api response messages & erros

---
+++ client/src/constants/index.js
@@ -0,0 +1,5 @@
+export default {
+  infected: 'You might be covid infected.',
+  checkError: 'Failed to process your request at this time. Please try again in another time.',
+  notInfected: 'You are not covid infected.'
+};
