291d88a0bc0d5de86bb0f54524d64ce44c70c3b8
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-01 01:31:56 +0200

    fix: update "--prod" with production and remove "--confirm"

--- client/bin/deploy.sh
+++ client/bin/deploy.sh
@@ -4,14 +4,14 @@ cd dist/ && vercel link --token=$VERCEL_TOKEN --project=$VERCEL_CLIENT_PROJECT -
 
 # set application env variables
 echo "Add app env variables"
-echo $VITE_BASE_URL | vercel env add VITE_BASE_URL --prod --token=$VERCEL_TOKEN 
-echo $VITE_AUTH0_DOMAIN | vercel env add VITE_AUTH0_DOMAIN --prod --token=$VERCEL_TOKEN
-echo $VITE_AUTH0_CLIENT_ID | vercel env add VITE_AUTH0_CLIENT_ID --prod --token=$VERCEL_TOKEN
-echo $VITE_AUTH0_REDIRECT_URI | vercel env add VITE_AUTH0_REDIRECT_URI --prod --token=$VERCEL_TOKEN
-echo $VITE_AUTH0_API_AUDIENCE | vercel env add VITE_AUTH0_API_AUDIENCE --prod --token=$VERCEL_TOKEN
-echo $VITE_AUT0_READ_API_SCOPE | vercel env add VITE_AUT0_READ_API_SCOPE --prod --token=$VERCEL_TOKEN
-echo $VITE_AUTH0_UPDATE_API_SCOPE | vercel env add VITE_AUTH0_UPDATE_API_SCOPE --prod --token=$VERCEL_TOKEN
+echo $VITE_BASE_URL | vercel env add VITE_BASE_URL production --token=$VERCEL_TOKEN 
+echo $VITE_AUTH0_DOMAIN | vercel env add VITE_AUTH0_DOMAIN production --token=$VERCEL_TOKEN
+echo $VITE_AUTH0_CLIENT_ID | vercel env add VITE_AUTH0_CLIENT_ID production --token=$VERCEL_TOKEN
+echo $VITE_AUTH0_REDIRECT_URI | vercel env add VITE_AUTH0_REDIRECT_URI production --token=$VERCEL_TOKEN
+echo $VITE_AUTH0_API_AUDIENCE | vercel env add VITE_AUTH0_API_AUDIENCE production --token=$VERCEL_TOKEN
+echo $VITE_AUT0_READ_API_SCOPE | vercel env add VITE_AUT0_READ_API_SCOPE production --token=$VERCEL_TOKEN
+echo $VITE_AUTH0_UPDATE_API_SCOPE | vercel env add VITE_AUTH0_UPDATE_API_SCOPE production --token=$VERCEL_TOKEN
 
 # deploy to project
 echo "Deploying client app"
-vercel deploy --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes --prod --confirm
+vercel deploy --token=$VERCEL_TOKEN --scope=$VERCEL_SCOPE --yes production 
