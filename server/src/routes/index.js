69052120d02c83c7d53d6c31a44ac87616393317
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-01 01:16:41 +0200

    fix: use auth0 middleware

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,12 +4,7 @@ import auth0Middleware from '../middleware/auth';
 
 const routes = Router();
 
-routes.use(
-  '/records',
-  auth0Middleware.validateAuth0Token,
-  auth0Middleware.checkScopes,
-  recordsRouter
-);
+routes.use('/records', auth0Middleware.validateAuth0Token, recordsRouter);
 
 routes.get('/', (_req, res) => {
   res.status(200).send('<h1 style="text-align:center;">Covid Tracker API</h1>');
