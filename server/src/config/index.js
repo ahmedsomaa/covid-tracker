3daba532776e97adcb314d5eb9f0ca79ebbf8c91
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-02-28 16:15:09 +0200

    feat: add auth0 domain & audience to config

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,11 +2,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { PORT, NODE_ENV, MONGO_USER, MONGO_USER_PASSWORD, MONGO_CLUSTER, MONGO_DATABASE } =
-  process.env;
+const {
+  PORT,
+  NODE_ENV,
+  MONGO_USER,
+  AUTH0_DOMAIN,
+  MONGO_CLUSTER,
+  MONGO_DATABASE,
+  AUTH0_API_AUDIENCE,
+  MONGO_USER_PASSWORD
+} = process.env;
 
 export default {
   port: PORT,
   env: NODE_ENV,
+  auth0: {
+    domain: AUTH0_DOMAIN,
+    audience: AUTH0_API_AUDIENCE
+  },
   dbUrl: `mongodb+srv://${MONGO_USER}:${MONGO_USER_PASSWORD}@${MONGO_CLUSTER}.mongodb.net/${MONGO_DATABASE}?retryWrites=true&w=majority`
 };
