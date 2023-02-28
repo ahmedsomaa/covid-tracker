da69d91f2505e8e9ffd90485fb91a76b8f7fe755
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-01 01:20:09 +0200

    refactor: use destructuring

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,8 +1,22 @@
+const {
+  VITE_BASE_URL,
+  VITE_AUTH0_DOMAIN,
+  VITE_AUTH0_CLIENT_ID,
+  VITE_AUTH0_API_AUDIENCE,
+  VITE_AUT0_READ_API_SCOPE,
+  VITE_AUTH0_UPDATE_API_SCOPE,
+  VITE_AUTH0_REDIRECT_URIVITE_AUTH0_REDIRECT_URI
+} = import.meta.env;
+
 export default {
-  domain: import.meta.env.VITE_AUTH0_DOMAIN,
-  baseUrl: import.meta.env.VITE_BASE_URL,
-  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-  audience: import.meta.env.VITE_AUTH0_API_AUDIENCE,
-  redirectUri: import.meta.env.VITE_AUTH0_REDIRECT_URI,
+  scope: {
+    read: VITE_AUT0_READ_API_SCOPE.replaceAll(',', ' '),
+    update: VITE_AUTH0_UPDATE_API_SCOPE.replaceAll(',', ' ')
+  },
+  domain: VITE_AUTH0_DOMAIN,
+  baseUrl: VITE_BASE_URL,
+  clientId: VITE_AUTH0_CLIENT_ID,
+  audience: VITE_AUTH0_API_AUDIENCE,
+  redirectUri: VITE_AUTH0_REDIRECT_URI,
   mapGeoUrl: 'https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json'
 };
